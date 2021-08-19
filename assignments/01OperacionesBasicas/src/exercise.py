328feb67795be938c94c5a8a31e2d7563055df60
fromerotec <fernandoromero@tec.mx>
2021-08-19 02:33:48 -0700

    commit 3

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    num1=int(input("Dame un número: "))
+    num2=int(input("Dame un número: "))
+    s = num1+num2
+    r = num1-num2
+    m = num1*num2
+    print("Suma:",s)
+    print("Resta:",r)
+    print("Multiplicación:",m)
 
 
 
